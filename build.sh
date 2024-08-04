f9e30a4ab8e9b8a5a72040d13d508f62eb2b953e
Stacey Vetzal <stacey@vetzal.com>
2024-08-04 07:56:08 -0400

    adjusting build to use output folder

--- build.sh
+++ build.sh
@@ -1,6 +1,20 @@
-lwasm --format=decb source.asm -oBSOD.BIN
+#!/bin/zsh
 
-rm bsod.dsk
-decb dskini -3 -n bsod.dsk
-decb copy -2 -b BSOD.BIN bsod.dsk,BSOD.BIN
+TARGET=output
+DISK_FILENAME=BSOD.BIN
+BINARY=$TARGET/$DISK_FILENAME
+DISK_IMAGE=$TARGET/bsod.dsk
+
+if [ ! -d $TARGET ]; then
+    mkdir $TARGET
+fi
+
+if [ -f $DISK_IMAGE ]; then
+    rm $DISK_IMAGE
+fi
+
+lwasm --format=decb source.asm -o$BINARY
+
+decb dskini -3 -n $DISK_IMAGE
+decb copy -2 -b $BINARY $DISK_IMAGE,$DISK_FILENAME
 
